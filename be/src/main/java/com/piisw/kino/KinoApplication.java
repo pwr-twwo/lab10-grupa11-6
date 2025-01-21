46ea9827de9dc42e1975a76bb6d6f8d958876d98
Pawel K <pkudlacik@proton.me>
2025-01-21 11:15:52 +0100

    jenkins test source code change 2

--- be/src/main/java/com/piisw/kino/KinoApplication.java
+++ be/src/main/java/com/piisw/kino/KinoApplication.java
@@ -8,7 +8,7 @@ public class KinoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KinoApplication.class, args);
-		System.out.println("----------------------------------KINO APP 1");
+		System.out.println("----------------------------------KINO APP 11");
 	}
 
 }
