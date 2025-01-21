7497642a2f3b8275f1add1b924590774fdceaa8d
Pawel K <pkudlacik@proton.me>
2025-01-21 10:13:30 +0100

    terraform - added ecs logger

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -91,6 +91,17 @@ resource "aws_ecs_task_definition" "app_task" {
                     value =  "prod"
                 }
                 ]
+            
+            logConfiguration = {
+                logDriver = "awslogs"
+                options = {
+                  "awslogs-create-group" = "true"
+                  "awslogs-group"  = "/ecs/"
+                  "awslogs-region" = "us-east-1"
+                  "awslogs-stream-prefix" = "devops"
+                }
+              }
+
 
         }
       ])
