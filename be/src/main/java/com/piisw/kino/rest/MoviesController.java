77f6def78bfb932ce5cb413df0f3eb844e2c3df1
Krzysztof Maciejewski <94606867+kdmaciejewski@users.noreply.github.com>
2025-01-22 12:28:24 +0100

    updating backend

--- be/src/main/java/com/piisw/kino/rest/MoviesController.java
+++ be/src/main/java/com/piisw/kino/rest/MoviesController.java
@@ -1,6 +1,7 @@
 package com.piisw.kino.rest;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.piisw.kino.dto.MovieTO;
 import com.piisw.kino.service.MoviesService;
+
 
 import lombok.RequiredArgsConstructor;
 
